684eb7a85e5b25b1e70c8cb75d4cf3de07af0cfb
Yihui Liao <44729383+yihuiliao@users.noreply.github.com>
2024-03-25 16:59:33 -0700

    initialize app and button

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Buttons from './Buttons'
+
+function App() {
+
+  return (
+    <>
+      <h2>Buttons</h2>
+      <Buttons />
+    </>
+  )
+}
+
+export default App
---
+++ src/Buttons.tsx
@@ -0,0 +1,30 @@
+import {useButton, AriaButtonOptions} from 'react-aria';
+import {useRef} from 'react';
+
+function Buttons() {
+  return (
+  <ButtonHook onPress={()=>alert('Button pressed')}>Button Hook</ButtonHook>
+  )
+}
+
+interface ButtonHookProps extends AriaButtonOptions<'button'> {
+  // children is a special prop in React that refers to the child elements passed to a react component
+  children?: string
+}
+
+function ButtonHook(props: ButtonHookProps) {
+  const {
+    children,
+    ...otherProps
+  } = props
+  const ref = useRef(null);
+  const {buttonProps} = useButton(otherProps, ref);
+  // console.log(children);
+  return (
+    <button {...buttonProps} ref={ref}>
+      {children}
+    </button>
+  )
+}
+
+export default Buttons;
