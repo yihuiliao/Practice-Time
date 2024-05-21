2b9af29bf25290a16e12275d203031ec27a4fa2e
Yihui Liao <44729383+yihuiliao@users.noreply.github.com>
2024-05-21 11:54:36 -0700

    move things around

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -28,10 +28,15 @@ async function run() {
   if ((/^update:?|^support:?|^feat(ure)?:?|^remove:?|^add:?|^fix:?|^bump:?|^docs:?|^pre-release:?|^revert:?/i).test(title)) {
     core.info('Success');
   } 
-  // else {
-  //   core.info('Sorry this failed, please read our PR naming guide to see how to correctly name your PR');
-  //   core.setFailed();
-  // }
+  else {
+    core.info('Sorry this failed, please read our PR naming guide to see how to correctly name your PR');
+    const {data} = await octokit.rest.issues.createComment({
+      ...context.repo,
+      issue_number: number,
+      body: 'This failed, please read our [PR naming guide](https://www.google.com/)'
+    });
+    core.setFailed();
+  }
 
   // console.log(...github.context.repo);
   // await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
@@ -40,11 +45,4 @@ async function run() {
   //   issue_number: number,
   //   body: 'Thanks',
   // })
-  const {data} = await octokit.rest.issues.createComment({
-    ...context.repo,
-    issue_number: number,
-    body: 'Thanks'
-  });
-
-  console.log(data);
 }
