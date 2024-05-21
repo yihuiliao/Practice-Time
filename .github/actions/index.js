07176cfe2e87b2b81cd872b48ffdd8176d66fe10
Yihui Liao <44729383+yihuiliao@users.noreply.github.com>
2024-05-21 10:29:00 -0700

    test comment

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -5,6 +5,10 @@ run();
 async function run() {
   // const title = github.context.payload.pull_request?.title;
   const title = core.getInput("pr-title")
+  const number = core.getInput("pr-number")
+  const token = core.getInput("repo-token")
+
+  const octokit = github.getOctokit(token)
 
   core.info(
     `🔎 Checking if the title of this PR "${title}" meets the requirements ...`
@@ -12,7 +16,14 @@ async function run() {
 
   if ((/^update:?|^support:?|^feat(ure)?:?|^remove:?|^add:?|^fix:?|^bump:?|^docs:?|^pre-release:?|^revert:?/i).test(title)) {
     core.info('Success');
-  } else {
-    core.setFailed(core.error);
-  }
+  } 
+  // else {
+  //   core.setFailed(core.error);
+  // }
+
+  await octokit.rest.issues.createComment({
+    ...github.context.repo,
+    issue_number: number,
+    body: 'Thanks'
+  });
 }
