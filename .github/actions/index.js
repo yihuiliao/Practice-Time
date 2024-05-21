573e796bb200592f75014a618c2e415d62c0d878
Yihui Liao <44729383+yihuiliao@users.noreply.github.com>
2024-05-21 10:38:56 -0700

    give up on commenting

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -17,13 +17,20 @@ async function run() {
   if ((/^update:?|^support:?|^feat(ure)?:?|^remove:?|^add:?|^fix:?|^bump:?|^docs:?|^pre-release:?|^revert:?/i).test(title)) {
     core.info('Success');
   } 
-  // else {
-  //   core.setFailed(core.error);
-  // }
+  else {
+    core.info('Sorry this failed, please read our PR naming guide to see how to correctly name your PR');
+    core.setFailed(core.error);
+  }
 
-  await octokit.rest.issues.createComment({
-    ...github.context.repo,
-    issue_number: number,
-    body: 'Thanks'
-  });
+  // console.log(...github.context.repo);
+  // await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+  //   ...github.context.repo,
+  //   issue_number: number,
+  //   body: 'Thanks',
+  // })
+  // await octokit.rest.issues.createComment({
+  //   ...github.context.repo,
+  //   issue_number: number,
+  //   body: 'Thanks'
+  // });
 }
